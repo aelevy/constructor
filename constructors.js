31b6620242735ec41dd1c78fc812d5922c557469
Andrew Levy <levyandrew44@yahoo.com>
2014-08-05 12:59:19 -0400

    initial commit

---
+++ constructors.js
@@ -0,0 +1,9 @@
+function Runner (name,speed,preference){
+  this.name=name;
+  this.speed=speed;
+  this.preference=preference;
+};
+
+var Steve = new Runner ("Steve",10,"track")
+var Bill = new Runner ("Bill",7,"trail")
+var Bob = new Runner ("Bob",5,"track")
